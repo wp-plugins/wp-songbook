46179af110814fb98b2f9f3862aec713a044a497
sjiamnocna <sjiamnocna@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-18 17:41:56 +0000

    Adding Zeta
    git-svn-id: https://plugins.svn.wordpress.org/wp-songbook/tags/2.0f@1201723 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wp-songbook.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Plugin Name: WP Songbook
+ * Description: Plugin for simple managing songs, song authors, lyrics and everything is needed for it.
+ * Version: 2.0.&zeta;
+ * Text Domain: wpsongbook
+ * Domain Path: /langs
+ * Author: Sjiamnocna
+ * Author URI: http://sjiamnocna.com/wpsongbook
+ */
+
+//tell if error occurs
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+define('WPSB_DIRNAME', 'wp-songbook-remake');
+define('WPSB_LANGDOM', 'wpsongbook');
+define('WPSB_VERSION', '2.0.&zeta;');
+define('WPSB_PLUGPATH', plugin_dir_path(__FILE__));
+
+include_once 'inc/func.functions.php';
+include_once 'inc/func.filters.php';
+include_once 'inc/class.ajax.php';
+include_once 'inc/class.base.php';
+include_once 'inc/class.posttypes.php';
+include_once 'inc/class.metabox.php';
+include_once 'inc/class.taxonomies.php';
+include_once 'inc/class.admin.php';
+include_once 'inc/class.hooks.php';
+include_once 'inc/class.mess.php';
